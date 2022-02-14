c14c7c180b770ced5e6737c52470c83c93dbe6a0
Michael Stickels <stickelsmt@gmail.com>
2022-02-14 10:11:14 -0800

    Add customizing documentation to code

--- Software/Macro_Keypad/Macro_Keypad.ino
+++ Software/Macro_Keypad/Macro_Keypad.ino
@@ -1,7 +1,20 @@
+/*
+
+  Macro_Keypad Firmware
+
+  Michael Stickels
+  
+  Last Updated: 02/14/2022
+
+  GPL-3.0 License
+
+*/
+
+// Import Libraries
 #include <Keypad_Matrix.h>
 #include <Keyboard.h>
 
-
+// Define keypress for keys 1-8
 #define KEY1 KEY_F13
 #define KEY2 KEY_F14
 #define KEY3 KEY_F15
@@ -12,92 +25,137 @@
 #define KEY8 KEY_F20
 #define ALT KEY_LEFT_ALT
 
-
+// Define keypad size and layout
 const byte ROWS = 2;
 const byte COLS = 4;
-
-
-// keypad layout
 const char keys[ROWS][COLS] = {{'1','2','3','4'},{'5','6','7','8'}};
 
+// Define Arduino pins
 const byte rowPins[ROWS] = {10, 16};
 const byte colPins[COLS] = {14, 15, 18, 19};
 
+// Initialize keypad object
+Keypad_Matrix kpd = Keypad_Matrix(makeKeymap (keys), rowPins, colPins, ROWS, COLS);
 
-Keypad_Matrix kpd = Keypad_Matrix( makeKeymap (keys), rowPins, colPins, ROWS, COLS );
+
+/*
+
+  To set custom macros, edit the case for the desired button in *BOTH* keyDown and keyUp.
+  Ensure that each Keyboard.press() is accompanied by a Keyboard.release() statement, or
+  the key will get "stuck down". Any reasonable combination of keys can be set, including
+  modifiers and multiple keypresses.
+
+  See https://www.arduino.cc/en/Reference/KeyboardModifiers for modifier keys 
+
+*/
 
 
+// Handler for keyDown event (key pressed down)
 void keyDown (const char which)
   {
   switch (which) {
+
+  // Key 1 press
   case '1':
     Keyboard.press(KEY1);
     break;
+
+  // Key 2 press
   case '2':
     Keyboard.press(KEY2);
     break;
-   case '3':
+
+  // Key 3 press
+  case '3':
     Keyboard.press(KEY3);
     break;
-   case '4':
+
+  // Key 4 press
+  case '4':
     Keyboard.press(KEY4);
     break;
-   case '5':
+
+  // Key 5 press
+  case '5':
     Keyboard.press(ALT);
     Keyboard.press(KEY5);
     break;
+
    case '6':
+   // Key 6 press
     Keyboard.press(KEY_LEFT_CTRL);
     Keyboard.press(KEY_LEFT_ALT);
-    Keyboard.press(206);
+    Keyboard.press(206); // print screen
     break;
-   case '7':
+
+  case '7':
+  // Key 7 press
     Keyboard.press(KEY_LEFT_ALT);
     Keyboard.press(KEY7);
     break;
-   case '8':
+
+  // Key 8 press
+  case '8':
     Keyboard.press(KEY8);
     break;
 }
   }
 
+// Handler for keyUp event (key unpressed)
 void keyUp (const char which)
   {
   switch (which) {
+
+  // Key 1 release
   case '1':
     Keyboard.release(KEY1);
     break;
+
+  // Key 2 release
   case '2':
     Keyboard.release(KEY6);
     break;
-   case '3':
+
+  // Key 3 release
+  case '3':
     Keyboard.release(KEY3);
     break;
-   case '4':
+
+  // Key 4 release
+  case '4':
     Keyboard.release(KEY4);
     break;
-   case '5':
+
+  // Key 5 release
+  case '5':
     Keyboard.release(ALT);
     Keyboard.release(KEY5);
     break;
-   case '6':
+
+  // Key 6 release
+  case '6':
     Keyboard.release(KEY_LEFT_CTRL);
     Keyboard.release(KEY_LEFT_ALT);
     Keyboard.release(206);
     break;
-   case '7':
+
+  // Key 7 release
+  case '7':
     Keyboard.release(KEY_LEFT_ALT);
     Keyboard.release(KEY7);
     break;
-   case '8':
+    
+  // Key 8 release
+  case '8':
     Keyboard.release(KEY8);
     break;
   }
 }
 
 
-
+// 
 void setup() {
+  // small delay for interrupting setup via write if needed
   delay(1000);
   kpd.begin ();
   kpd.setKeyDownHandler (keyDown);
